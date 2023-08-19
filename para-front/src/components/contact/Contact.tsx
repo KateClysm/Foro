8483f76eee72ba340a88ea33e6c5337d93fac958
ValentinaBS <vbsanchez16@gmail.com>
2023-08-19 14:09:09 -0300

    feat: form validation and styles

--- para-front/src/components/contact/Contact.tsx
+++ para-front/src/components/contact/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent } from 'react';
+import React, { FormEvent, useState } from 'react';
 import './contact.css'
 import instagramIcon from '../../assets/contact/instagram.png'
 import facebookIcon from '../../assets/contact/facebook.png'
@@ -8,6 +8,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faEnvelope, faMessage } from '@fortawesome/free-solid-svg-icons';
 
 const Contact: React.FC = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [nameTouched, setNameTouched] = useState(false);
+    const [emailTouched, setEmailTouched] = useState(false);
+    const [messageTouched, setMessageTouched] = useState(false);
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isEmailValid = email === '' || emailRegex.test(email);
+
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
     };
@@ -23,7 +34,7 @@ const Contact: React.FC = () => {
                         <FontAwesomeIcon icon={faUser} className='contact-icon' />
                         <label htmlFor='name'>
                             Your name
-                            <span className="contact-required">*</span>
+                            <span className='contact-required'>*</span>
                         </label>
                     </div>
                     <input
@@ -33,15 +44,20 @@ const Contact: React.FC = () => {
                         placeholder='Jane Doe'
                         className='contact-input'
                         required
+                        onBlur={() => setNameTouched(true)}
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
                     />
-                    <span className="contact-validation">
-                        Oh no!
-                    </span>
+                    {nameTouched && name.length <= 4 && (
+                        <span className='contact-validation button-text'>
+                            Your name must be more than 4 characters long
+                        </span>
+                    )}
                     <div className='form-label'>
                         <FontAwesomeIcon icon={faEnvelope} className='contact-icon' />
                         <label htmlFor='email'>
                             Your email
-                            <span className="contact-required">*</span>
+                            <span className='contact-required'>*</span>
                         </label>
                     </div>
                     <input
@@ -51,12 +67,25 @@ const Contact: React.FC = () => {
                         placeholder='janedoe@email.com'
                         className='contact-input'
                         required
+                        onBlur={() => setEmailTouched(true)}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
+                    {emailTouched && !isEmailValid && (
+                        <span className='contact-validation button-text'>
+                            Your email address must be in format: email@example.com
+                        </span>
+                    )}
+                    {emailTouched && email === '' && (
+                        <span className='contact-validation button-text'>
+                            Email is required
+                        </span>
+                    )}
                     <div className='form-label'>
                         <FontAwesomeIcon icon={faMessage} className='contact-icon' />
                         <label htmlFor='message'>
                             Your message
-                            <span className="contact-required">*</span>
+                            <span className='contact-required'>*</span>
                         </label>
                     </div>
                     <textarea
@@ -64,8 +93,16 @@ const Contact: React.FC = () => {
                         name='message'
                         placeholder='I wanted to ask about...'
                         required
+                        onBlur={() => setMessageTouched(true)}
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
                     >
                     </textarea>
+                    {messageTouched && message === '' && (
+                        <span className='contact-validation button-text'>
+                            You can't send an empty message
+                        </span>
+                    )}
                     <input className='contact-submit' type='submit' value='Send' />
                 </form>
                 <div className='contact-socials'>
