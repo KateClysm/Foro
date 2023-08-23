f3ab4d3005410f16a701105ec1af15597f0cbf2b
KateClysm <kate.clysm@gmail.com>
2023-08-23 16:34:16 -0300

    Estilos del NotFound, grid para el botón, recolorización de la flecha. Ruta del boton hacia Home, comentado de código en desuso en el about-us y en contact. Modificación de la estructura de MainContent y PrincipalPosts. -Maria Monchot

--- para-front/src/pages/main-content/MainContent.tsx
+++ para-front/src/pages/main-content/MainContent.tsx
@@ -8,36 +8,32 @@ import PostsContent from '../posts-content/PostsContent';
 import AsideRight from '../../components/aside-right/AsideRight';
 import AboutUs from '../../components/about-us/AboutUs';
 import NotFoundPage from '../../components/not-found/NotFound';
+import Contact from '../../components/contact/Contact';
 
 const MainContent: React.FC = () => {
   return (
-    <Routes>
-      
-          <Route path="/" element={
-            <>
-                <NavBar/>
-                <div className='main-content'>
-                    <AsideLeft/>
-                    <PostsContent/>
-                    <AsideRight/>
-                </div>
-                <Footer/>
-            </>
-          } />
+   <>
+        <NavBar/>
+        <div className='main-content'>
+            <AsideLeft/>
+            <Routes>
+                
+                <Route path="/" element={
+                  <>
+                      <PostsContent/>
+                      <AsideRight/>
+                  </>
+                } />
 
-          <Route path="/about" element={
-             <>
-             <NavBar/>
-             <div className='main-content'>
-                 <AsideLeft/>
-                 <AboutUs/>
-             </div>
-             <Footer/>
-            </>
-          }/>
+                <Route path="/about" element={<AboutUs/>}/>
+                <Route path="/contact" element={<Contact/>}/>
 
-          <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </div>
+       
+        <Footer/>
+   </>
   );
 };
 export default MainContent;
