a95a831c57fcf36e71cc26751636c110631fbbb2
Ludmila Agostinelli <luddagostinelli1@gmail.com>
2023-08-25 17:00:42 -0300

    fixed: html & css login. feature: login component

---
+++ para-front/src/components/login/Login.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import './login.css';
+
+const Login: React.FC = () => {
+    return (
+    <form>
+    
+        <div className="container-login">
+
+            <div className="login-center">
+
+               <div className="div-login">
+                 <p> Log In </p>
+               </div>
+
+                <div className="email-login">
+                  <label>Please introduce your email:</label>
+                   <input type="email" name="email-login"/>
+                </div>
+
+               <div className="password-login">
+                  <label>Please introduce your password:</label>
+                  <input type="password" name="password-login"/>
+               </div>
+
+               <div className="check-login">
+                 <input type="checkbox" name="stay-login"/>
+                 <label> Stay logged in</label>
+                 
+               </div>
+
+               <div className="submit-login">
+                 <input type="submit" value="Login"/>
+                </div>
+
+              <div className="login-divider">
+                  <div className="divider"></div>
+               </div>
+
+               <div className="register-option">
+                   <p>You want to create an account? 
+                        <a class="register" href="./register.html">
+                           Register
+                        </a>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+   </form>
+)
+}
+export default Login
