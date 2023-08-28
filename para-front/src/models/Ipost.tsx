97c29e12aca01bf4f801da8920533474aab94fde
Nana Villarroel <icarus851005@gmail.com>
2023-08-28 20:20:52 -0300

    agregado de interfaces post, user y reactions

--- para-front/src/models/Ipost.tsx
+++ para-front/src/models/Ipost.tsx
@@ -1,14 +1,20 @@
 import { PostTypeSelection } from './PostTypes';
 
-export interface IPost {
-    title: string;
-    text: React.ReactNode;
-    userImage?: string;
-    userName: string;
-    userTime: number;
-    comments: number;
-    likes: number;
-    views: number;
-    imagePost?: string;
-    typePost: PostTypeSelection;
-  }
+export interface Post {
+  title: string;
+  text: React.ReactNode;
+  imagePost?: string;
+  typePost: PostTypeSelection;
+};
+
+export interface User {
+  username: string;
+  userImage?: string;
+  userTime: number;
+};
+
+export interface Reactions {
+  likes: number;
+  comments: number;
+  views: number;
+};
