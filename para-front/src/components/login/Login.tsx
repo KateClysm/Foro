c6d674ca24c471b4a826f0d4cd3f55f0b3dd9c4f
ValentinaBS <vbsanchez16@gmail.com>
2023-08-28 21:36:43 -0300

    comments

--- para-front/src/components/login/Login.tsx
+++ para-front/src/components/login/Login.tsx
@@ -3,50 +3,49 @@ import './login.css';
 import { NavLink } from 'react-router-dom';
 
 const Login: React.FC = () => {
-    return (
+  return (
     <form>
-    
-        <div className="container-login">
 
-            <div className="login-center">
+      <div className="container-login"> {/* Div extra, podría ser el form */}
 
-               <div className="div-login">
-                 <p> Log In </p>
-               </div>
+        <div className="login-center"> {/* Div extra, se puede posicionar desde el form */}
 
-                <div className="email-login">
-                  <label>Please introduce your email:</label>
-                   <input type="email" name="email-login"/>
-                </div>
+          <div className="div-login"> {/* Div extra, los divs no deberían contener una sola etiqueta */}
+            <p> Log In </p>
+          </div>
 
-               <div className="password-login">
-                  <label>Please introduce your password:</label>
-                  <input type="password" name="password-login"/>
-               </div>
+          <div className="email-login">
+            <label htmlFor='email'>Please introduce your email:</label> {/* htmlFor y id para relacionar label con input */}
+            <input id='email' type="email" name="email-login" placeholder='janedoe@email.com' required/>
+          </div>
 
-               <div className="check-login">
-                 <input type="checkbox" name="stay-login"/>
-                 <label> Stay logged in</label>
-                 
-               </div>
+          <div className="password-login">
+            <label>Please introduce your password:</label>
+            <input type="password" name="password-login" />
+          </div>
 
-               <div className="submit-login">
-                 <input type="submit" value="Login"/>
-                </div>
+          <div className="check-login">
+            <input type="checkbox" name="stay-login" />
+            <label>Stay logged in</label>
+          </div>
 
-              <div className="login-divider">
-                  <div className="divider"></div>
-               </div>
+          <div className="submit-login"> {/* Div extra, los divs no deberían contener una sola etiqueta */}
+            <input type="submit" value="Login" />
+          </div>
 
-               <div className="register-option">
-                   <p>You want to create an account? 
-                        <NavLink to="/register">Register</NavLink>
-                    </p>
-                </div>
-            </div>
+          <div className="login-divider"> {/* Div extra, los divs no deberían contener una sola etiqueta */}
+            <div className="divider"></div>
+          </div>
 
+          <div className="register-option"> {/* Div extra, los divs no deberían contener una sola etiqueta */}
+            <p>You want to create an account?
+              <NavLink to="/register">Register</NavLink>
+            </p>
+          </div>
         </div>
-   </form>
-);
+
+      </div>
+    </form>
+  );
 };
 export default Login;
