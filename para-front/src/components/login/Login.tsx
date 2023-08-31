28497312c2e4f6a419faa7d5316f309c0615c310
ValentinaBS <vbsanchez16@gmail.com>
2023-08-31 16:27:23 -0300

    refactor: form styles

--- para-front/src/components/login/Login.tsx
+++ para-front/src/components/login/Login.tsx
@@ -3,32 +3,32 @@ import './login.css';
 import { NavLink } from 'react-router-dom';
 
 const Login: React.FC = () => {
-    return (
-       <form action="#" className="login-form">
-           <h1> Log In </h1>
+  return (
+    <form action="#" className="login-form">
+      <h1>Log In</h1>
 
-           <fieldset className="email-login">
-             <label htmlFor="email-login"> Email</label>
-             <input type="email" id="email-login" name="email" required/>
-            </fieldset>
+      <div className="email-login">
+        <label className='post-text' htmlFor="email-login">Email</label>
+        <input type="email" id="email-login" name="email" placeholder='janedoe@email.com' required />
+      </div>
 
-           <fieldset className="pass-login">
-             <label htmlFor="pass-login"> Password</label>
-             <input type="password" id="pass-login" name="pass" required/>
-           </fieldset>
+      <div className="pass-login">
+        <label className='post-text' htmlFor="pass-login">Password</label>
+        <input type="password" id="pass-login" name="pass" required />
+      </div>
 
-           <fieldset className="stay-login"> 
-             <input type="checkbox" id="stay-login" name="stay-login"/>
-             <label htmlFor="stay-login"> Stay logged in </label>              
-           </fieldset>
+      <div className="stay-login">
+        <input type="checkbox" id="stay-login" name="stay-login" />
+        <label htmlFor="stay-login">Stay logged in</label>
+      </div>
 
-           <button className="login-button" type="submit">Submit</button>
+      <button className="login-button button-text" type="submit">Submit</button>
 
-          <div className="register-option">
-             <p>You want to create an account? </p>
-             <NavLink to="/register">Register</NavLink>
-          </div>
-       </form>
-    )
+      <div className="register-option">
+        <p>You want to create an account? </p>
+        <NavLink to="/register" className="register-option-link">Register</NavLink>
+      </div>
+    </form>
+  )
 }
 export default Login;
