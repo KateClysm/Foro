1df02ec02ff0e3595779fd703689fed52ab56adc
Ludmila Agostinelli <luddagostinelli1@gmail.com>
2023-09-03 13:21:11 -0300

    .

--- para-front/src/components/login/Login.tsx
+++ para-front/src/components/login/Login.tsx
@@ -29,22 +29,23 @@ const Login: React.FC = () => {
 
     return (
        <form action="#" className="login-form" onSubmit={handleSubmit}>
-           <h1> Log In </h1>
+           <h1>Log In</h1>
 
-           <fieldset className="email-login">
-             <label htmlFor="email-login"> Email </label>
+           <div className="email-login">
+             <label className='post-text' htmlFor="email-login">Email</label>
              <input 
              type="email" 
              id="email-login" 
-             name="email" 
+             name="email"
+             placeholder='janedoe@email.com'
              required
              value={email}
              onChange={(e) => setEmail (e.target.value)}
              />
-            </fieldset>
+            </div>
 
-           <fieldset className="pass-login">
-             <label htmlFor="pass-login"> Password</label>
+           <div className="pass-login">
+             <label className='post-text' htmlFor="pass-login">Password</label>
              <input 
              type="password" 
              id="pass-login" 
@@ -53,18 +54,18 @@ const Login: React.FC = () => {
              value={password}
              onChange={(e) => setPassword (e.target.value)}
              />
-           </fieldset>
+           </div>
 
-           <fieldset className="stay-login"> 
-             <input type="checkbox" id="stay-login" name="stay-login"/>
+           <div className="stay-login"> 
+             <input type="checkbox" id="stay-login" name="stay-login" />
              <label htmlFor="stay-login"> Stay logged in </label>              
-           </fieldset>
+           </div>
 
-           <button className="login-button" type="submit">Submit</button>
+           <button className="login-button button-text" type="submit">Submit</button>
 
           <div className="register-option">
              <p>You want to create an account? </p>
-             <NavLink to="/register">Register</NavLink>
+             <NavLink to="/register" className="register-option-link">Register</NavLink>
           </div>
        </form>
     )
