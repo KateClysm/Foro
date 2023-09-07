3938b6057270763eba9b5c06953a02ff770f20bb
Maria Monchot <kateclysm@gmail.com>
2023-09-07 12:07:44 -0300

    Eliminado de carpetas de testeo. -Maria Monchot

--- para-front/src/components/login/Login.tsx
+++ para-front/src/components/login/Login.tsx
@@ -1,30 +1,41 @@
-import React, { FormEvent, useState } from 'react';
-import './login.scss';
-import { NavLink } from 'react-router-dom';
+import { FormEvent, useContext, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
+import "./login.scss";
 
-const Login: React.FC = () => {
-  const [email, setEmail] = useState ('');
-  const [password, setPassword] = useState ('');
-  const [emailLogged, setEmailLogged] = useState (false)
-  const [passwordLogged, setPasswordLogged] = useState (false)
+const Login = () => {
+  //campos del formulario
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const emailValid = email === '' || emailRegex.test(email);
+  //notificaciones
+  const [err, setErr] = useState(null);
 
-  const handleSubmit = (e: FormEvent) => {
+  const navigate = useNavigate()
+
+  const handleChange = (e: FormEvent) => {
+    e.preventDefault()
+    setEmail('')
+    setPassword('')
+  };
+
+  const { login } = useContext(AuthContext);
+
+  const handleLogin = async (e:FormEvent) => {
     e.preventDefault();
-
-    if( email == null || email.length == 0 || !emailRegex.test(email) ) {
-      return("email inválido")}
-
-    if(password == null || password.length == 0 || /^\s+$/.test(password)) {
-      return ("password inválida")
+    try {
+      await login({email, password});
+      navigate("/")
+    } catch (err:any) {
+      setErr(err.response.data);
     }
-  }
-    return (
-       <form action="#" className="login-form" onSubmit={handleSubmit}>
-           <h1>Log In</h1>
+  };
 
+  console.log(err);
+
+  return (
+    <form action="#" className="login-form" onSubmit={handleChange}>
+           <h1>Log In</h1>
            <div className="email-login">
              <label className='post-text' htmlFor="email-login">Email</label>
              <input 
@@ -33,16 +44,10 @@ const Login: React.FC = () => {
              name="email"
              placeholder='janedoe@email.com'
              required
-             onBlur={() => setEmailLogged (true)}
              value={email}
              onChange={(e) => setEmail (e.target.value)}
              />
             </div>
-            {emailLogged && !emailValid &&(
-            <span className='email-validation'>
-              Your email address must be in format: email@example.com
-            </span>
-           )}
 
            <div className="pass-login">
              <label className='post-text' htmlFor="pass-login">Password</label>
@@ -51,29 +56,21 @@ const Login: React.FC = () => {
              id="pass-login" 
              name="password" 
              required
-             onBlur={() => setPasswordLogged (true)}
              value={password}
              onChange={(e) => setPassword (e.target.value)}
              />
            </div>
-           {passwordLogged && password.length <= 8 &&(
-        <span className='password-validation'>
-          Your password must have at least 8 characters
-        </span>
-      )}
+           
 
-           <div className="stay-login"> 
-             <input type="checkbox" id="stay-login" name="stay-login" />
-             <label htmlFor="stay-login"> Stay logged in </label>              
-           </div>
-
-           <button className="login-button button-text" type="submit">Submit</button>
+           <button className="login-button button-text" type="submit" onClick={handleLogin}>Login</button>
+           {err && <p>{err}</p>}
 
           <div className="register-option">
              <p>You want to create an account? </p>
              <NavLink to="/register" className="register-option-link">Register</NavLink>
           </div>
        </form>
-    )
-}
+    );
+};
+
 export default Login;
