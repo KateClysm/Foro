c7411e1c345b01ad91ddbcb388dbe1f8538fc2d6
Maria Monchot <kateclysm@gmail.com>
2023-09-17 21:30:58 -0300

    Arreglos del register -Maria Monchot

--- para-front/src/pages/register/Register.tsx
+++ para-front/src/pages/register/Register.tsx
@@ -65,135 +65,137 @@ const Register: React.FC = () => {
   console.log(err);
 
   return (
-    <form action="#" className="register-form" onSubmit={handleChange}>
-      <h1>Register</h1>
-        <div className='column-left'>
-          {/* username */}
-          <div className="username-register">
-            <label htmlFor="username-register">
-              Introduce an username
-              <span className='username-required'>*</span>
-            </label>
-            <input
-              type="username"
-              id="username-register"
-              name="username"
-              required
-              onBlur={() => setUsernameTouched(true)}
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder='JaneDoe123' />
+   <div className="container-form">
+         <form action="#" className="register-form" onSubmit={handleChange}>
+          <h1>Register</h1>
+            <div className='column-left'>
+              {/* username */}
+              <div className="username-register">
+                <label htmlFor="username-register">
+                  Introduce an username
+                  <span className='username-required'>*</span>
+                </label>
+                <input
+                  type="username"
+                  id="username-register"
+                  name="username"
+                  required
+                  onBlur={() => setUsernameTouched(true)}
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  placeholder='JaneDoe123' />
+              </div>
+              {usernameTouched && !isUsernameValid && (
+                <span className='username-validation'>
+                  Your username must have at least 3 characters.
+                </span>
+              )}
+              {/* name */}
+              <div className="name-register">
+                <label htmlFor="name-register">
+                  Introduce a name
+                  <span className='name-required'>*</span>
+                </label>
+                <input
+                  type="name"
+                  id="name-register"
+                  name="name"
+                  required
+                  onBlur={() => setNameTouched(true)}
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder='Jane Doe' />
+              </div>
+              {nameTouched && !isNameValid && (
+                <span className='name-validation'>
+                  Your name must have at least 3 characters
+                </span>
+              )}
+            </div>
+
+            <div className='column-rigth'>
+              {/* password */}
+              <div className="password-register">
+                <label htmlFor="password-register">
+                  Introduce a password</label>
+                <input
+                  type="password"
+                  id="password-register"
+                  name="password"
+                  required
+                  onBlur={() => setPasswordTouched(true)}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              {passwordTouched && !isPasswordLenghtValid && (
+                <span className='password-validation'>
+                  Your password must have at least 8 characters.
+                </span>
+              )}
+              {passwordTouched && !passwordNotBlank && (
+                <span className='password-validation'>
+                  You can't leave this field empty.
+                </span>
+              )}
+
+              {/* confirm password */}
+              <div className="confirm-password">
+                <label htmlFor="confirm-password">Confirm password</label>
+                <input
+                  type="password"
+                  id="confirm-password"
+                  name="confirm-password"
+                  required
+                  onBlur={() => setConfirmPasswordTouched(true)}
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </div>
+              {confirmPasswordTouched && !passwordMatch && passwordNotBlank && (
+                <span className='password-validation'>
+                  Your passwords do not match.
+                </span>
+              )}
+            </div>
+
+          <div className='bottom-elements'>
+            {/* email */}
+            <div className="email-register">
+              <label htmlFor="email-register">
+                Introduce an email
+                <span className='email-required'>*</span>
+              </label>
+              <input
+                type="email"
+                id="email-register"
+                name="email"
+                placeholder='janedoe@email.com'
+                required
+                onBlur={() => setEmailTouched(true)}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)} />
+              {emailTouched && !emailValid && (
+                <span className='email-validation'>
+                  Your email address must be in format: email@example.com
+                </span>
+              )}
+            </div>
+
+            {/* submit */}
+            <button className="register-button-btn" type="submit" onClick={handleClick} >Confirm</button>
+            {err && <p>{err}</p>}
+            {notification && <p>{notification}</p>}
+
+            {/* login */}
+            <div className="logIn-option">
+              <p>You already have an account?</p>
+              <NavLink to="/login" className="logIn-option-link">Login</NavLink>
+            </div>
           </div>
-          {usernameTouched && !isUsernameValid && (
-            <span className='username-validation'>
-              Your username must have at least 3 characters.
-            </span>
-          )}
-          {/* name */}
-          <div className="name-register">
-            <label htmlFor="name-register">
-              Introduce a name
-              <span className='name-required'>*</span>
-            </label>
-            <input
-              type="name"
-              id="name-register"
-              name="name"
-              required
-              onBlur={() => setNameTouched(true)}
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder='Jane Doe' />
-          </div>
-          {nameTouched && !isNameValid && (
-            <span className='name-validation'>
-              Your name must have at least 3 characters
-            </span>
-          )}
-        </div>
 
-        <div className='column-rigth'>
-          {/* password */}
-          <div className="password-register">
-            <label htmlFor="password-register">
-              Introduce a password</label>
-            <input
-              type="password"
-              id="password-register"
-              name="password"
-              required
-              onBlur={() => setPasswordTouched(true)}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          {passwordTouched && !isPasswordLenghtValid && (
-            <span className='password-validation'>
-              Your password must have at least 8 characters.
-            </span>
-          )}
-          {passwordTouched && !passwordNotBlank && (
-            <span className='password-validation'>
-              You can't leave this field empty.
-            </span>
-          )}
-
-          {/* confirm password */}
-          <div className="confirm-password">
-            <label htmlFor="confirm-password">Confirm password</label>
-            <input
-              type="password"
-              id="confirm-password"
-              name="confirm-password"
-              required
-              onBlur={() => setConfirmPasswordTouched(true)}
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-          </div>
-          {confirmPasswordTouched && !passwordMatch && passwordNotBlank && (
-            <span className='password-validation'>
-              Your passwords do not match.
-            </span>
-          )}
-        </div>
-
-      <div className='bottom-elements'>
-        {/* email */}
-        <div className="email-register">
-          <label htmlFor="email-register">
-            Introduce an email
-            <span className='email-required'>*</span>
-          </label>
-          <input
-            type="email"
-            id="email-register"
-            name="email"
-            placeholder='janedoe@email.com'
-            required
-            onBlur={() => setEmailTouched(true)}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)} />
-          {emailTouched && !emailValid && (
-            <span className='email-validation'>
-              Your email address must be in format: email@example.com
-            </span>
-          )}
-        </div>
-
-        {/* submit */}
-        <button className="register-button-btn" type="submit" onClick={handleClick} >Confirm</button>
-        {err && <p>{err}</p>}
-        {notification && <p>{notification}</p>}
-
-        {/* login */}
-        <div className="logIn-option">
-          <p>You already have an account?</p>
-          <NavLink to="/login" className="logIn-option-link">Login</NavLink>
-        </div>
-      </div>
-
-    </form>
+        </form>
+   </div>
   );
 };
 export default Register;
